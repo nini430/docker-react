cde7f48ae60c59687568150a21a2659613673b7e
nini430 <ninigogatishvili1@gmail.com>
2024-01-31 16:35:31 +0400

    modify

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-         holy fuck:)!!!!!!!!!!!!!!!!
+         some changes
         </a>
       </header>
     </div>
